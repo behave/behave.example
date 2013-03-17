dc5512769a7986ebc14c49b7e35c0118c56639be
jenisys <jens_engel@web.de>
2013-03-17 13:07:57 +0100

    Introduce project_sitecustomize

--- bin/behave_run.py
+++ bin/behave_run.py
@@ -11,30 +11,31 @@ DEBUG = False
 USE_COLOCATED_BEHAVE = False
 
 # ----------------------------------------------------------------------------
-# SETUP PATHS:
+# PROJECT-SPECIFIC SETUP PATHS:
 # ----------------------------------------------------------------------------
-# HERE = os.path.dirname(sys.argv[0])
-HERE = os.path.dirname(__file__)
-PYTHON_LIBDIR = os.path.normpath(os.path.join(HERE, "..", "lib",
-        "python%s" % sys.version_info[0] ))
-BEHAVE_ZIP  = os.path.join(PYTHON_LIBDIR, "behave.zip")
-BEHAVE_HOME = os.path.normpath(os.path.join(HERE, "..", "..", "behave"))
+import project_sitecustomize
 
-if DEBUG:
-    print "BEHAVE: HERE=%s" % HERE
-    print "BEHAVE: BEHAVE_HOME=%s" % BEHAVE_HOME
-
-# -- LOCAL-PATH-SETUP: Use local libs if possible.
-if os.path.isdir(PYTHON_LIBDIR):
-    sys.path.insert(0, os.path.abspath(PYTHON_LIBDIR))
-if os.path.exists(BEHAVE_ZIP):
-    # sys.stdout.write("USING-BUNDLE: %s\n" % os.path.basename(BEHAVE_ZIP))
-    if DEBUG: sys.stdout.write("USING-BUNDLE: %s\n" % BEHAVE_ZIP)
-    sys.path.insert(0, os.path.realpath(BEHAVE_ZIP))
-if USE_COLOCATED_BEHAVE and os.path.isdir(BEHAVE_HOME):
-    if DEBUG: sys.stdout.write("USING-LOCAL: %s\n" % BEHAVE_HOME)
-    sys.path.insert(0, os.path.abspath(os.path.join(BEHAVE_HOME)))
-
+# HERE = os.path.dirname(__file__)
+# PYTHON_LIBDIR = os.path.normpath(os.path.join(HERE, "..", "lib",
+#         "python%s" % sys.version_info[0] ))
+# BEHAVE_ZIP  = os.path.join(PYTHON_LIBDIR, "behave.zip")
+# BEHAVE_HOME = os.path.normpath(os.path.join(HERE, "..", "..", "behave"))
+#
+# if DEBUG:
+#     print "BEHAVE: HERE=%s" % HERE
+#     print "BEHAVE: BEHAVE_HOME=%s" % BEHAVE_HOME
+#
+# # -- LOCAL-PATH-SETUP: Use local libs if possible.
+# if os.path.isdir(PYTHON_LIBDIR):
+#     sys.path.insert(0, os.path.abspath(PYTHON_LIBDIR))
+# if os.path.exists(BEHAVE_ZIP):
+#     # sys.stdout.write("USING-BUNDLE: %s\n" % os.path.basename(BEHAVE_ZIP))
+#     if DEBUG: sys.stdout.write("USING-BUNDLE: %s\n" % BEHAVE_ZIP)
+#     sys.path.insert(0, os.path.realpath(BEHAVE_ZIP))
+# if USE_COLOCATED_BEHAVE and os.path.isdir(BEHAVE_HOME):
+#     if DEBUG: sys.stdout.write("USING-LOCAL: %s\n" % BEHAVE_HOME)
+#     sys.path.insert(0, os.path.abspath(os.path.join(BEHAVE_HOME)))
+#
 # ----------------------------------------------------------------------------
 # BEHAVE-PATCHES:
 # ----------------------------------------------------------------------------
---
+++ bin/project_sitecustomize.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+PROJECT WORKSPACE-SPECIFIC SETUP:
+---------------------------------
+
+Adds project-specific local packages under library path as site-package dir.
+See also :py:module:`site`.
+
+REQUIREMENTS:
+  * Must be colocated to the scripts in "bin/" directory to simplify import.
+"""
+
+from __future__ import print_function
+import os.path
+import site
+import sys
+
+DEBUG = False
+
+# ----------------------------------------------------------------------------
+# SETUP PATH UTILS:
+# ----------------------------------------------------------------------------
+def project_workspace_addsitedir(sitedir):
+    """
+    Similar to site.addsitedir() but prefers new sitedir over existing ones.
+    Therefore, prefers local packages over installed packages.
+
+    .. note::
+        This allows to support *.pth files and zip-/egg-imports
+        similar to an installed site-packages directory.
+    """
+    assert os.path.isdir(sitedir)
+    next_package_pos = len(sys.path)
+    site.addsitedir(sitedir)
+
+    # -- POST-PROCESS: Move new packages from end to begin of sys.path list.
+    pos = 0
+    new_packages = sys.path[next_package_pos:]
+    del sys.path[next_package_pos:]
+    sys.path[pos:pos] = new_packages
+
+# ----------------------------------------------------------------------------
+# PROJECT-SPECIFIC PATHS:
+# ----------------------------------------------------------------------------
+py_version_major = sys.version_info[0]
+HERE = os.path.dirname(__file__)
+TOPA = os.path.abspath(os.path.join(HERE, ".."))
+PYTHON_LIBDIR = os.path.join(TOPA, "lib", "python%s" % py_version_major)
+
+if DEBUG:
+    print("usercustomize: Use PYTHON_LIBDIR=%s" % PYTHON_LIBDIR)
+
+# ----------------------------------------------------------------------------
+# SETUP PATHS:
+# ----------------------------------------------------------------------------
+if os.path.isdir(PYTHON_LIBDIR):
+    project_workspace_addsitedir(PYTHON_LIBDIR)
+sys.path.insert(0, TOPA)
+
+# ----------------------------------------------------------------------------
+# DIAGNOSTIC MAIN:
+# ----------------------------------------------------------------------------
+if __name__ == "__main__":
+    print("sys.path[%d]:" % len(sys.path))
+    for index, p in enumerate(sys.path):
+        print("  %3d  %s" % (index, p))
