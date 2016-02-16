5513c73f519f139b5b0686f66472c1422787324a
jenisys <jenisys@users.noreply.github.com>
2016-02-17 00:11:25 +0100

    Increment VERSION to 1.2.5.1 (was: 1.2.5)

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ INSTALL_REQUIRES = requirements
 # -----------------------------------------------------------------------------
 setup(
     name="behave.example",
-    version="1.2.5",
+    version="1.2.5.1",
     url="http://github.com/jenisys/behave.example",
     author="Jens Engel",
     author_email="Jens_Engel@nowhere.net",
